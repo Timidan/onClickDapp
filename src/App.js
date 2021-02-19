9b9a26f6adfadb0e4311005e2eb7cf1e9cb8ba67
Jeremiah <newcreationjerry96@gmail.com>
2021-02-19 20:41:22 +0000

    first commit for one Click Dapp

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import React, { Component } from "react";
+import onClickABI from "./contracts/onClickDapp.json";
+import getWeb3 from "./getWeb3";
+import {Alert} from 'reactstrap';
+
+import "./App.css";
+
+class App extends Component {
+  state = { 
+    storageValue: 0, 
+    web3: null, 
+    accounts: null, 
+    contract: null,
+    visible: false,
+    currentAccount: null,
+    currrentNetwork: null
+   };
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+      this.setState({
+        web3
+      })
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+      const currentAccount = accounts[0];
+      this.setState({
+        currentAccount
+      })
+
+      // Get the contract instance.
+      const currrentNetwork = await web3.eth.net.getNetworkType();
+      const contractAddress = "0x6405Bc85B15de208AE0728fb980e0Ca2d3517E81";
+      let contract;
+
+      if(currrentNetwork === "rinkeby") {
+        contract = new web3.eth.Contract(onClickABI, contractAddress);
+        this.setState({
+          contract
+        })
+
+        console.log(currrentNetwork, "current Network");
+      }
+      else {
+        this.setState({
+          visible: true
+        })
+      }
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
+
+  toggle(){
+    this.setState({
+        visible: false
+    })
+}
+
+  getAirdropInBscHandler=async()=> {
+    const result = await this.state.contract.methods.getAirdropInBsc().send({
+      from: this.state.currentAccount
+    })
+    console.log("result", result)
+  }
+
+  
+
+  render() {
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
+    return (
+      <div className="App">
+
+        <nav className="link2Faucet">
+          <h3 className="brand">Approval DApp</h3>
+
+          <p> Click <a target="_blank" rel="noreferrer" href="https://faucet.rinkeby.io/">here</a> to get rinkeby faucet </p> 
+
+        </nav>
+        <Alert isOpen={this.state.visible} toggle={this.toggle.bind(this)}>
+                     <div className="alert-content">
+                     <img alt="key" src="https://image.freepik.com/free-vector/shield-with-key-icon_24911-9270.jpg" /> 
+                         <p className="alert-text"> Kindly switch to Rinkeby Network on Metamask and restart </p> 
+                     </div>
+        </Alert>
+
+        <div className="userInteraction">
+          <h2> Get AirDrop</h2>
+          <button
+          onClick={this.getAirdropInBscHandler}>claim airdrop</button>
+
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
